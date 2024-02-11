ef843845d850f33d1092c080f145ae609090803d
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-02-11 22:21:10 +0200

    done

--- models/user.py
+++ models/user.py
@@ -1,6 +1,10 @@
+#!/usr/bin/python3
+"""A USER Module"""
 from models.base_model import BaseModel
 
+
 class User(BaseModel):
+    """The User Needed Attributes"""
     email = ""
     password = ""
     first_name = ""
