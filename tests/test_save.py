5c4ff5a3160f4e7f23383df5e5bb94ac82c31cbe
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-02-12 10:51:25 +0200

    done

---
+++ tests/test_save.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+""" A Module To Test my Project"""
+import unittest
+from unittest.mock import patch
+from datetime import datetime
+from models.base_model import BaseModel
+
+
+class TestBaseModelSaveMethod(unittest.TestCase):
+    @patch('models.storage')
+    def test_save_updates_updated_at(self, mock_storage):
+        # Create an instance of BaseModel
+        bm = BaseModel()
+
+        # Save the current updated_at value for later comparison
+        original_updated_at = bm.updated_at
+
+        # Call the save method
+        bm.save()
+
+        # Check that updated_at has been updated
+        self.assertNotEqual(bm.updated_at, original_updated_at)
+        self.assertIsInstance(bm.updated_at, datetime)
+
+        # Check that storage.save() was called once
+        mock_storage.save.assert_called_once()
+
+
+if __name__ == '__main__':
+    unittest.main()
