6fe3549d40033fc33b86c676e8d46ce85ab17528
C4ll-0f-Du5ty <allemhamed98@gmail.com>
2024-02-12 11:06:39 +0200

    done

--- tests/test_base_model.py
+++ tests/test_base_model.py
@@ -3,6 +3,7 @@
 import unittest
 from datetime import datetime
 from models.base_model import BaseModel
+from unittest.mock import patch
 
 
 class TestBaseModel(unittest.TestCase):
@@ -36,6 +37,24 @@ class TestBaseModel(unittest.TestCase):
                          r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}Z?$')
         self.assertRegex(model_dict['updated_at'],
                          r'\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}\.\d{6}Z?$')
+
+    @patch('models.storage')
+    def test_save(self, mock_storage):
+        # Create an instance of BaseModel
+        bm = BaseModel()
+
+        # Save the current updated_at value for later comparison
+        original_updated_at = bm.updated_at
+
+        # Call the save method
+        bm.save()
+
+        # Check that updated_at has been updated
+        self.assertNotEqual(bm.updated_at, original_updated_at)
+        self.assertIsInstance(bm.updated_at, datetime)
+
+        # Check that storage.save() was called once
+        mock_storage.save.assert_called_once()
 
 
 if __name__ == '__main__':
